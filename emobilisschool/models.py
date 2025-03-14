09ca4e9ff37d5033016eef5df445f87b57f75dd4
morrinegitari <morrinegitari@gmail.com>
2025-03-14 20:08:34 +0300

    Update models.py
    remember to make migrations and migrate to configure models

--- emobilisschool/models.py
+++ emobilisschool/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Students(models.Model):
+    name=models.CharField(max_length=50)
+    adm=models.IntegerField()
+    date=models.DateField()
+    form=models.Charfield(max_length=50)
+
+
+
+    def __str__(self):
+        return self.name
+class Teachers(models.Model):
+    name=models.CharField(max_length=15)
+    age=models.IntegerField()
+    email=models.EmailField()
+    department=models.CharField(max_length=50)
+    phone=models.CharField(max_length=15)
+
+    def __str__(self):
+        return self.name+" "+self.status
+
+class Parents(models.Model):
+    firstname=models.CharField(max_length=50)
+    lastname=models.CharField(max_length=50)
+    phonenumber=models.IntegerField()
+    email=models.EmailField()
+  
+
+    def __str__(self):
+        return self.firstname+" "+self.lastname
+
+
